d5cd61c35dec7a5f1bf8aa27f066cc71fd408d8b
brittonhayes <brittonhayes@live.com>
2024-06-24 22:49:39 -0700

    fix: updated pages from notion

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -5,7 +5,12 @@ const userConfig: UserConfig = {
     manual: false,
     page_url:
       "https://brittonhayes.notion.site/f9327e6820a64364908934e6723c0b24?v=464df212c74a46e7b438b7c4c2e09a95&pvs=74",
-    pages: [],
+    pages: [
+      {
+        page_id: "7fc57929beb44658bca01bc11bcff2f3",
+        target_folder: ".",
+      },
+    ],
     databases: [
       {
         database_id: "f9327e6820a64364908934e6723c0b24",
