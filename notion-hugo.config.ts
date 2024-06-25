5e0ba2545163a8a50f466d9811c1dfdb42eef2ea
brittonhayes <brittonhayes@live.com>
2024-06-24 23:04:34 -0700

    fix: notion homepage url

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -4,7 +4,7 @@ const userConfig: UserConfig = {
   mount: {
     manual: false,
     page_url:
-      "https://brittonhayes.notion.site/f9327e6820a64364908934e6723c0b24?v=464df212c74a46e7b438b7c4c2e09a95&pvs=74",
+      "https://brittonhayes.notion.site/Blog-5c1704f40f3346daadc1bf5d9a781c0f?pvs=4",
     pages: [
       {
         page_id: "7fc57929beb44658bca01bc11bcff2f3",
