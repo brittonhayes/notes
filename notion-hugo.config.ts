c59938ea7a5d2ed34188db69ae8bd882ff7b719f
brittonhayes <brittonhayes@live.com>
2024-06-24 22:38:29 -0700

    feat: updated database id and site url

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -1,30 +1,18 @@
-import { UserConfig } from "./src/config"
+import { UserConfig } from "./src/config";
 
 const userConfig: UserConfig = {
-    mount: {
-        manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
-        pages: [
-            // {
-            //     page_id: '<page_id>',
-            //     target_folder: 'path/relative/to/content/folder'
-            // }
-            {
-                page_id: '45eb121158b9489480ec000fd25c812b',
-                target_folder: '.'
-            }
-        ],
-        databases: [
-            // {
-            //     database_id: '<database_id>',
-            //     target_folder: 'path/relative/to/content/folder'
-            // }
-            {
-                database_id: 'b7b1816c05ec464391c8c111fa242985',
-                target_folder: '.'
-            }
-        ],
-    }
-}
+  mount: {
+    manual: false,
+    page_url:
+      "https://brittonhayes.notion.site/f9327e6820a64364908934e6723c0b24?v=464df212c74a46e7b438b7c4c2e09a95&pvs=74",
+    pages: [],
+    databases: [
+      {
+        database_id: "f9327e6820a64364908934e6723c0b24",
+        target_folder: ".",
+      },
+    ],
+  },
+};
 
 export default userConfig;
